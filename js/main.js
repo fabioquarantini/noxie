8547368f2b1dedc6bb6998b7dc42da69c99563f2
Fabio Quarantini <wlospam@gmail.com>
2013-05-13 19:19:49 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+/*  ==========================================================================
+	Author: Fabio Quarantini - @febba
+	==========================================================================  */
+
+var siteInit = {
+
+	DOMready: function() {
+
+		if ($('.modal-iframe').length > 0) {
+			siteInit.modalIframe();
+		}
+
+		if ($('.modal-gallery').length > 0) {
+			siteInit.modalGallery();
+		}
+
+	},
+
+
+	/* Colorbox iframe */
+
+	modalIframe: function() {
+
+		$(".modal-iframe").colorbox({
+			iframe: true,
+			transition: "elastic",
+			speed: 400,
+			opacity: 0.6,
+			initialWidth: 50,
+			initialHeight: 50,
+			width: "80%",
+			height: "80%"
+		});
+
+	},
+
+
+	/* Colorbox image gallery */
+
+	modalGallery: function() {
+
+		$(".modal-gallery").colorbox({
+			transition: "elastic",
+			speed: 400,
+			opacity: 0.6,
+			slideshow: true,
+			slideshowSpeed: 4000,
+			initialWidth: 50,
+			initialHeight: 50,
+			className: "colorbox-gallery"
+		});
+
+	}
+
+};
+
+
+/*  ==========================================================================
+	Document ready
+	==========================================================================  */
+
+$(document).ready(function() {
+	siteInit.DOMready();
+});
