1f2ff8ad3ced0967a3b09c3d2ac3317bc1462703
Fabio Quarantini <wlospam@gmail.com>
2013-05-28 16:16:47 +0200

    Cleaned spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,69 +1,68 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-	// Metadata.
-	pkg: grunt.file.readJSON('package.json'),
+	// Project configuration.
+	grunt.initConfig({
+		// Metadata.
+		pkg: grunt.file.readJSON('package.json'),
 
-	concat: {	//https://github.com/gruntjs/grunt-contrib-concat
-		options: {
-			separator: ';'
-		},
-		dist: {
-			src: [ 'js/plugins/*.js'],
-			dest: 'js/plugins.js'
-		}
-	},
-	cssmin: {	// https://github.com/gruntjs/grunt-contrib-cssmin
-		// todo: attualmente il file main.css viene sovrascritto 
-		combine: {
-			files: {
-				'css/main.css': ['css/main.min.css', 'css/normalize.min.css']
+		concat: { //https://github.com/gruntjs/grunt-contrib-concat
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: ['js/plugins/*.js'],
+				dest: 'js/plugins.js'
 			}
 		},
-		minify: {
-			expand: true,
-			cwd: 'css/',
-			src: ['*.css'],
-			dest: 'css/',
-			ext: '.min.css'
-		}
-	},
-	// image-min https://github.com/gruntjs/grunt-contrib-imagemin
-	imagemin: {
-		dist: {
-			options: {
-				optimizationLevel: 3
+		cssmin: { // https://github.com/gruntjs/grunt-contrib-cssmin
+			// todo: attualmente il file main.css viene sovrascritto 
+			combine: {
+				files: {
+					'css/main.css': ['css/main.min.css', 'css/normalize.min.css']
+				}
 			},
-			files: [{
+			minify: {
 				expand: true,
-				cwd: 'img/',
-				src: '*',
-				dest: 'img'
-			}]
-		}
-	},
-	uglify: {
-		my_target: {
-			options: {
-				sourceMapRoot: 'js/plugins/',
-				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> + <%= grunt.template.today("yyyy-mm-dd") %> */'
-			},
-			files: {
-				'js/plugins.js': ['js/plugins/*.js']
+				cwd: 'css/',
+				src: ['*.css'],
+				dest: 'css/',
+				ext: '.min.css'
+			}
+		},
+		// image-min https://github.com/gruntjs/grunt-contrib-imagemin
+		imagemin: {
+			dist: {
+				options: {
+					optimizationLevel: 3
+				},
+				files: [{
+						expand: true,
+						cwd: 'img/',
+						src: '*',
+						dest: 'img'
+					}
+				]
+			}
+		},
+		uglify: {
+			my_target: {
+				options: {
+					sourceMapRoot: 'js/plugins/',
+					banner: '/*! <%= pkg.name %> - v<%= pkg.version %> + <%= grunt.template.today("yyyy-mm-dd") %> */'
+				},
+				files: {
+					'js/plugins.js': ['js/plugins/*.js']
+				}
 			}
 		}
-	}
-});
+	});
 
-grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-cssmin');
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-
-
-  //grunt.registerTask('default', [ 'concat']);
+	//grunt.registerTask('default', [ 'concat']);
 
 };
