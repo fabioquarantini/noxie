f2474d3a79b86c0cbd6b2b18d45c6e3bf053b964
daniel <iridiumweb@gmail.com>
2013-07-31 12:33:49 +0200

    Added grunt-contrib-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,69 +2,69 @@
 	Grunt configuration
 	==========================================================================  */
 
-module.exports = function(grunt) {
+	module.exports = function(grunt) {
 
-	grunt.initConfig({
+		grunt.initConfig({
 
-		/* Reads dependencies from package.json */
-		pkg: grunt.file.readJSON('package.json'),
-
-
-		/* [ grunt concat ] Concatenate javascript files (https://github.com/gruntjs/grunt-contrib-concat) */
-		concat: {
-			options: {
-				separator: ';'
-			},
-			dist: {
-				src: ['js/plugins/*.js'],
-				dest: 'js/plugins.js'
-			}
-		},
+			/* Reads dependencies from package.json */
+			pkg: grunt.file.readJSON('package.json'),
 
 
-		/* [ grunt cssmin:combine ] [ grunt cssmin:minify ] Combines and minifies css (https://github.com/gruntjs/grunt-contrib-cssmin) */
-		cssmin: {
-			combine: {
-				files: {
-					'css/main.css': ['css/*.min.css']
+			/* [ grunt concat ] Concatenate javascript files (https://github.com/gruntjs/grunt-contrib-concat) */
+			concat: {
+				options: {
+					separator: ';'
+				},
+				dist: {
+					src: ['js/plugins/*.js'],
+					dest: 'js/plugins.js'
 				}
 			},
-			minify: {
-				expand: true,
-				cwd: 'css/',
-				src: ['*.css'],
-				dest: 'css/',
-				ext: '.min.css'
-			}
-		},
 
 
-		/* [ grunt imagemin ] Images optimization (https://github.com/gruntjs/grunt-contrib-imagemin) */
-		imagemin: {
-			dist: {
-				options: {
-					optimizationLevel: 3,
-					progressive: true
+			/* [ grunt cssmin:combine ] [ grunt cssmin:minify ] Combines and minifies css (https://github.com/gruntjs/grunt-contrib-cssmin) */
+			cssmin: {
+				combine: {
+					files: {
+						'css/main.css': ['css/*.min.css']
+					}
 				},
-				files: [{
+				minify: {
+					expand: true,
+					cwd: 'css/',
+					src: ['*.css'],
+					dest: 'css/',
+					ext: '.min.css'
+				}
+			},
+
+
+			/* [ grunt imagemin ] Images optimization (https://github.com/gruntjs/grunt-contrib-imagemin) */
+			imagemin: {
+				dist: {
+					options: {
+						optimizationLevel: 3,
+						progressive: true
+					},
+					files: [{
 						expand: true,
 						cwd: 'img/',
 						src: '*',
 						dest: 'img/optimized/'
 					}
-				]
-			}
-		},
+					]
+				}
+			},
 
 
-		/* [ grunt sass:dev ] Compiles main.scss in development mode (https://github.com/gruntjs/grunt-contrib-sass) */
-		/* [ grunt sass:dist ] Compiles main.scss in distribution mode */
-		sass: {
-			dev: {
-				files: {
-					'css/main.css': 'scss/main.scss'
-				},
-				options: {
+			/* [ grunt sass:dev ] Compiles main.scss in development mode (https://github.com/gruntjs/grunt-contrib-sass) */
+			/* [ grunt sass:dist ] Compiles main.scss in distribution mode */
+			sass: {
+				dev: {
+					files: {
+						'css/main.css': 'scss/main.scss'
+					},
+					options: {
 					sourcemap: false,  // Requires Sass 3.3.0, which can be installed with gem install sass --pre
 					style: 'expanded',
 					lineNumbers: true,
@@ -79,7 +79,7 @@ module.exports = function(grunt) {
 					style: 'compressed'
 				}
 			}
-		}
+		},
 
 
 		/* [ grunt uglify ] Javascript plugins compressor (https://github.com/gruntjs/grunt-contrib-uglify) */
@@ -94,18 +94,28 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		watch: {
+			css: {
+				files: 'scss/*.scss',
+				tasks: ['sass'],
+				options: {
+					livereload: true,
+				}
+			}
+		}		
 		
 	});
 
 
-	/* Load tasks */
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-uglify');	
-	
-	/* Register tasks */
-	grunt.registerTask('default', [ 'sass:dev']);
+/* Load tasks */
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+/* Register tasks */
+grunt.registerTask('default', [ 'sass:dev']);
 
 };
