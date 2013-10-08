b010169c8a9623f2094c564a1ab58515be96d6b5
daniel <iridiumweb@gmail.com>
2013-10-08 17:05:33 +0200

    windows task runner

---
+++ hangar.bat
@@ -0,0 +1,24 @@
+@ECHO off
+cls
+:start
+ECHO.
+ECHO 1 Run Dev task
+ECHO 2 Run Build task
+ECHO 3 Run Weinre task
+set /p choice=Choose which task hangar should run.
+if '%choice%'=='1' goto dev
+if '%choice%'=='2' goto build
+if '%choice%'=='3' goto weinre
+ECHO.
+goto start
+:dev
+grunt 
+goto end
+:build
+grunt build
+goto end
+:weinre
+grunt weinre
+goto end
+:end
+pause
